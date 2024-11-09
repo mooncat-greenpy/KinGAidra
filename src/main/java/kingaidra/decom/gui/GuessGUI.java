3429c5fc2eb830ed4423ff637db7041b269cb6b1
mooncat-greenpy <mooncat.greenpy@gmail.com>
2024-11-09 21:52:30 +0900

    Change Configure window from checkbox to table

--- src/main/java/kingaidra/decom/gui/GuessGUI.java
+++ src/main/java/kingaidra/decom/gui/GuessGUI.java
@@ -1,11 +1,19 @@
 package kingaidra.decom.gui;
 
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
+import javax.swing.DefaultCellEditor;
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.DefaultTableModel;
 
 import ghidra.program.model.address.Address;
 import kingaidra.decom.DecomDiff;
@@ -22,24 +30,71 @@ public class GuessGUI extends JPanel {
 
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        for (Model m : models) {
-            JCheckBox cb = new JCheckBox(m.get_name());
-            cb.setSelected(guess.get_model_status(m));
-            cb.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    JCheckBox src = (JCheckBox) e.getSource();
-                    for (Model ml : models) {
-                        if (!ml.get_name().equals(src.getText())) {
-                            continue;
-                        }
-                        guess.set_model_status(ml, src.isSelected());
-                        break;
+        DefaultTableModel table_model =
+                new DefaultTableModel(new Object[] {"ON/OFF", "Name", "Script"}, 0) {
+                    @Override
+                    public Class<?> getColumnClass(int columnIndex) {
+                        return columnIndex == 0 ? Boolean.class : super.getColumnClass(columnIndex);
                     }
+                };
+        table_model.addTableModelListener(new TableModelListener() {
+            @Override
+            public void tableChanged(TableModelEvent e) {
+                if (e.getType() != TableModelEvent.UPDATE) {
+                    return;
                 }
-            });
-            add(cb);
+                int row = e.getFirstRow();
+                int column = e.getColumn();
+                if (column != 0) {
+                    return;
+                }
+
+                boolean status = (boolean) table_model.getValueAt(row, 0);
+                String name = (String) table_model.getValueAt(row, 1);
+                for (Model ml : models) {
+                    if (!ml.get_name().equals(name)) {
+                        continue;
+                    }
+                    guess.set_model_status(ml, status);
+                    break;
+                }
+            }
+        });
+        for (Model m : models) {
+            table_model
+                    .addRow(new Object[] {guess.get_model_status(m), m.get_name(), m.get_script()});
         }
+
+        JTable table = new JTable(table_model);
+        table.getColumnModel().getColumn(0).setCellEditor(new DefaultCellEditor(new JCheckBox()));
+        add(new JScrollPane(table));
+
+        JPanel btn_panel = new JPanel();
+        Dimension button_size = new Dimension(140, 30);
+
+        JButton add_btn = new JButton("Add Column");
+        add_btn.setPreferredSize(button_size);
+        add_btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                table_model.addRow(new Object[] {Boolean.FALSE, "", ""});
+            }
+        });
+        JButton del_btn = new JButton("Delete Column");
+        del_btn.setPreferredSize(button_size);
+        del_btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int row_idx = table.getSelectedRow();
+                if (row_idx == -1) {
+                    return;
+                }
+                table_model.removeRow(row_idx);
+            }
+        });
+        btn_panel.add(add_btn);
+        btn_panel.add(del_btn);
+        add(btn_panel);
     }
 
     public DecomDiff[] run_guess(Address addr) {
