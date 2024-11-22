782dd6340257e4e5be21073a8e47793aecad46de
mooncat-greenpy <mooncat.greenpy@gmail.com>
2024-11-23 01:42:30 +0900

    Add prompt to guess datatypes

--- ghidra_scripts/kingaidra_chatgptlike.py
+++ ghidra_scripts/kingaidra_chatgptlike.py
@@ -29,8 +29,8 @@ def extract_json(data, pre, post):
     except ValueError:
         return None
 
-def extract_json_bf(data):
-    if "{" not in data or "}" not in data or "new_func_name" not in data:
+def extract_json_bf(data, must_key):
+    if "{" not in data or "}" not in data or must_key not in data:
         return None
 
     length = len(data)
@@ -38,7 +38,7 @@ def extract_json_bf(data):
         for post in reversed(range(length + 1)):
             if post <= pre:
                 break
-            if "{" not in data[pre:post] or "}" not in data[pre:post] or "new_func_name" not in data[pre:post]:
+            if "{" not in data[pre:post] or "}" not in data[pre:post] or must_key not in data[pre:post]:
                 break
             try:
                 return json.loads(data[pre:post])
@@ -46,14 +46,19 @@ def extract_json_bf(data):
                 pass
     return None
 
+def request_chatgptlike(data):
+    req = urllib2.Request(
+        URL,
+        json.dumps(data),
+        {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + API_KEY,
+            "User-Agent": "Python",
+        }
+    )
+    return json.loads(urllib2.urlopen(req).read())
 
-def main():
-    consumer_list = currentProgram.getConsumerList()
-    service = consumer_list[0].getService(kingaidra.decom.KinGAidraDecomTaskService)
-    state = getState()
-
-    diff = service.get_task(state.getEnvironmentVar("KEY"))
-
+def guess_func_name_var_name(diff):
     data = {
         "model": MODEL,
         "messages": [
@@ -88,29 +93,19 @@ The format is as follows.
         ]
     }
 
-    req = urllib2.Request(
-        URL,
-        json.dumps(data),
-        {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + API_KEY,
-            "User-Agent": "Python",
-        }
-    )
-    response = json.loads(urllib2.urlopen(req).read())
+    response = request_chatgptlike(data)
 
     if "error" in response:
-        service.commit_task_error(json.dumps(response))
-        return
+        return json.dumps(response), {}, {}
     data = response["choices"][0]["message"]["content"]
 
     json_data = extract_json(data, "```json\n", "```")
     if not json_data:
-        json_data = extract_json_bf(data)
+        json_data = extract_json_bf(data, "new_func_name")
         if not json_data:
-            service.commit_task_error(data)
-            return
+            return data, {}, {}
 
+    new_func = json_data["new_func_name"]
     params = {}
     for i in json_data.get("parameters", []):
         if "orig_param_name" in i and "new_param_name" in i:
@@ -119,7 +114,76 @@ The format is as follows.
     for i in json_data.get("variables", []):
         if "orig_var_name" in i and "new_var_name" in i:
             vars[i["orig_var_name"]] = i["new_var_name"]
-    service.commit_task(state.getEnvironmentVar("KEY"), json_data["new_func_name"], params, vars, {})
+    return new_func, params, vars
+
+def guess_datatype(diff):
+    data = {
+        "model": MODEL,
+        "messages": [
+            {
+                "role": "user",
+                "content":"""I have decompiled C code that contains various data type issues due to the decompilation process. I need your help to review the code and make the necessary corrections to the data types. Please go over the code and make these adjustments to improve the accuracy of the data types.
+```cpp
+%s
+```
+The changes should be output in JSON format.
+```json
+[
+    {
+        "new_datatype": "new datatype name",
+        "orig_datatype": "original datatype name",
+        "var_name": "variable name"
+    },
+    ...
+]
+```""" % diff.get_src_code()
+            }
+        ]
+    }
+
+    response = request_chatgptlike(data)
+
+    if "error" in response:
+        return {}
+    data = response["choices"][0]["message"]["content"]
+
+    json_data = extract_json(data, "```json\n", "```")
+    if not json_data:
+        json_data = extract_json_bf(data, "[")
+        if not json_data:
+            return {}
+
+    datatypes = {}
+    for i in json_data:
+        if "var_name" in i and "new_datatype" in i:
+            datatypes[i["var_name"]] = i["new_datatype"]
+    return datatypes
+
+def main():
+    consumer_list = currentProgram.getConsumerList()
+    service = consumer_list[0].getService(kingaidra.decom.KinGAidraDecomTaskService)
+    state = getState()
+    key = state.getEnvironmentVar("KEY")
+
+    diff = service.get_task(key)
+
+    try:
+        new_func, params, vars = guess_func_name_var_name(diff)
+    except Exception as e:
+        service.commit_task_error(key, str(e))
+        return
+    if new_func is "":
+        service.commit_task_error(key, new_func)
+        return
+
+    try:
+        datatypes = guess_datatype(diff)
+    except Exception:
+        datatypes = {}
+    if not datatypes:
+        service.commit_task(key, new_func, params, vars, {})
+
+    service.commit_task(key, new_func, params, vars, datatypes)
 
 if __name__ == "__main__":
     main()
