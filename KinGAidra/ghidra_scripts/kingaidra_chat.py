a76e4076eb871bc627d365d0205c7740c33c46c5
mooncat-greenpy <mooncat.greenpy@gmail.com>
2025-04-12 14:50:07 +0900

    Add new functions for function calling
    - get_function_list
    - get_asm_by_name
    - get_decompiled_code_by_name
    - get_strings
    - get_ref_to

--- KinGAidra/ghidra_scripts/kingaidra_chat.py
+++ KinGAidra/ghidra_scripts/kingaidra_chat.py
@@ -64,6 +64,20 @@ def add_tools(data):
         {
             "type": "function",
             "function": {
+                "name": "get_function_list",
+                "description": "Retrieve functions in the binary.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                    },
+                    "additionalProperties": False
+                },
+                "strict": True
+            }
+        },
+        {
+            "type": "function",
+            "function": {
                 "name": "get_called_function",
                 "description": "Retrieve functions that are called by the specified function.",
                 "parameters": {
@@ -112,6 +126,22 @@ def add_tools(data):
         {
             "type": "function",
             "function": {
+                "name": "get_asm_by_name",
+                "description": "Retrieve the assembly code of the specified function.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "name": {"type": "string"},
+                    },
+                    "required": ["name"],
+                    "additionalProperties": False
+                },
+                "strict": True
+            }
+        },
+        {
+            "type": "function",
+            "function": {
                 "name": "get_decompiled_code",
                 "description": "Retrieve the decompiled code of the specified function in C language.",
                 "parameters": {
@@ -120,6 +150,22 @@ def add_tools(data):
                         "address": {"type": "number"},
                     },
                     "required": ["address"],
+                    "additionalProperties": False
+                },
+                "strict": True
+            }
+        },
+        {
+            "type": "function",
+            "function": {
+                "name": "get_decompiled_code_by_name",
+                "description": "Retrieve the decompiled code of the specified function in C language.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "name": {"type": "string"},
+                    },
+                    "required": ["name"],
                     "additionalProperties": False
                 },
                 "strict": True
@@ -195,6 +241,36 @@ def add_tools(data):
                 },
                 "strict": True
             }
+        },
+        {
+            "type": "function",
+            "function": {
+                "name": "get_strings",
+                "description": "Retrieve strings in the binary.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                    },
+                    "additionalProperties": False
+                },
+                "strict": True
+            }
+        },
+        {
+            "type": "function",
+            "function": {
+                "name": "get_ref_to",
+                "description": "Returns a list of references to the specified address.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "address": {"type": "number"},
+                    },
+                    "required": ["address"],
+                    "additionalProperties": False
+                },
+                "strict": True
+            }
         }
     ]
 
@@ -208,18 +284,38 @@ def handle_tool_call(tool_call, ghidra):
         content = "Addresses list.\n"
         for func in same_name_funcs:
             content += "- %d\n" % (func.getEntryPoint().getOffset())
+    elif func_name == "get_function_list":
+        func_itr = currentProgram.getListing().getFunctions(True)
+        content = "Functions list.\n"
+        while func_itr.hasNext():
+            func = func_itr.next()
+            content += "- [%d]: %s\n" % (func.getEntryPoint().getOffset(), func.getName())
     elif func_name == "get_called_function":
         func = ghidra.get_func(ghidra.get_addr(args["address"]))
+        called_func_list = func.getCalledFunctions(TaskMonitor.DUMMY)
         content = "Functions list.\n"
-        for calling_func in func.getCalledFunctions(TaskMonitor.DUMMY):
-            content += "- [%d]: %s\n" % (calling_func.getEntryPoint().getOffset(), calling_func.getName())
+        for called_func in called_func_list:
+            content += "- [%d]: %s\n" % (called_func.getEntryPoint().getOffset(), called_func.getName())
     elif func_name == "get_calling_function":
         func = ghidra.get_func(ghidra.get_addr(args["address"]))
+        calling_func_list = func.getCallingFunctions(TaskMonitor.DUMMY)
         content = "Functions list.\n"
-        for calling_func in func.getCallingFunctions(TaskMonitor.DUMMY):
+        for calling_func in calling_func_list:
             content += "- [%d]: %s\n" % (calling_func.getEntryPoint().getOffset(), calling_func.getName())
+    elif func_name == "get_asm_by_name":
+        same_name_funcs = ghidra.get_func(args["name"])
+        if len(same_name_funcs) == 1:
+            content = ghidra.get_asm(same_name_funcs[0].getEntryPoint())
+        else:
+            content = "Failed"
     elif func_name == "get_asm":
         content = ghidra.get_asm(ghidra.get_addr(args["address"]))
+    elif func_name == "get_decompiled_code_by_name":
+        same_name_funcs = ghidra.get_func(args["name"])
+        if len(same_name_funcs) == 1:
+            content = ghidra.get_decom(same_name_funcs[0].getEntryPoint())
+        else:
+            content = "Failed"
     elif func_name == "get_decompiled_code":
         content = ghidra.get_decom(ghidra.get_addr(args["address"]))
     elif func_name == "refactoring":
@@ -245,6 +341,16 @@ def handle_tool_call(tool_call, ghidra):
             content = "Success"
         else:
             content = "Failed"
+    elif func_name == "get_strings":
+        data_list = ghidra.get_strings()
+        content = "Strings list.\n"
+        for data in data_list:
+            content += "- [%d]: %s\n" % (data.getAddress().getOffset(), data.getDefaultValueRepresentation())
+    elif func_name == "get_ref_to":
+        ref_list = ghidra.get_ref_to(ghidra.get_addr(args["address"]))
+        content = "Reference address.\n"
+        for ref in ref_list:
+            content += "- %d\n" % (ref.getFromAddress().getOffset())
     return content
 
 def main():
