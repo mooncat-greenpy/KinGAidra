6af9ce6a2b3fd9b4b5a8fb641aa821c613aa6ccb
mooncat-greenpy <mooncat.greenpy@gmail.com>
2025-07-19 08:43:36 +0900

    Fix tests

--- KinGAidra/src/test/java/kingaidra/testutil/ModelDummy.java
+++ KinGAidra/src/test/java/kingaidra/testutil/ModelDummy.java
@@ -165,7 +165,7 @@ public class ModelDummy implements Model, Serializable {
                                 "        \"var_name\": \"in_stack_00000004\"\n" +
                                 "    }\n" +
                                 "]");
-        } else if (task_type == TaskType.DECOM_RESOLVE_DATATYPE && convo.get_msg(convo.get_msgs_len() - 1).contains(" PROCESSENTRY32W ")) {
+        } else if (task_type == TaskType.DECOM_RESOLVE_DATATYPE && convo.get_msg(convo.get_msgs_len() - 1).contains(" PROCESSENTRY32W.")) {
             convo.add_assistant_msg("Here is the PROCESSENTRY32W structure in C language, along with its dependent data types and structures, for 32-bit systems:\n" +
                                 "```\n" +
                                 "typedef wchar_t WCHAR;\n" +
@@ -198,7 +198,7 @@ public class ModelDummy implements Model, Serializable {
                                 "\n" +
                                 "Also, `WCHAR` is a wide character type, which is a 16-bit Unicode character on Windows.\n" +
                                 "");
-        } else if (task_type == TaskType.DECOM_RESOLVE_DATATYPE && convo.get_msg(convo.get_msgs_len() - 1).contains(" PROCESSENTRY32 ")) {
+        } else if (task_type == TaskType.DECOM_RESOLVE_DATATYPE && convo.get_msg(convo.get_msgs_len() - 1).contains(" PROCESSENTRY32.")) {
             convo.add_assistant_msg("Here is the `PROCESSENTRY32` structure written in C for a 32-bit environment, including any necessary dependent data types and structures:\n" +
                                 "\n" +
                                 "```c\n" +
